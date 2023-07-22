759e4316dde67b8c0be955fcfeef1a1ddff45c43
tyange <usun16@gmail.com>
2023-07-22 20:41:59 +0900

    added quiz question model

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
