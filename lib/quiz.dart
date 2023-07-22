0321ef2569b34c04658bd8b8135e57663f2925c3
tyange <usun16@gmail.com>
2023-07-22 19:43:38 +0900

    using if statement

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,6 +22,12 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
@@ -36,9 +42,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
