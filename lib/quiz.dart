65565b6bdbe8c83345266fba6ae249791c0f08fd
tyange <usun16@gmail.com>
2023-07-23 10:08:20 +0900

    making results screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_quiz_app/start_screen.dart';
 import 'package:flutter_complete_quiz_app/questions_screen.dart';
 import 'package:flutter_complete_quiz_app/data/questions.dart';
+import 'package:flutter_complete_quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -29,7 +30,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -41,6 +42,12 @@ class _QuizState extends State<Quiz> {
     if (activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
       );
     }
 
---
+++ lib/results_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_complete_quiz_app/data/questions.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'questions_index': i,
+        'question': questions[i].text,
+        'correctAnswer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You answered X out fo Y questions correctly!'),
+            SizedBox(
+              height: 30,
+            ),
+            Text('List of answers and questions...'),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Restart Quiz!'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
